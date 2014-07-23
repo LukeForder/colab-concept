d3774991da9abc6e0e2083f1020c09f50235fd0f
Luke Forder <luke.forder@gmail.com>
2014-07-23 23:30:41 +0200

    initial commit

---
+++ ColabConcept.Web/Models/Product.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ColabConcept.Web.Models
+{
+    public class Product
+    {
+        public Guid Id
+        {
+            get;
+            set;
+        }
+
+        public Guid Version
+        {
+            get;
+            set;
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public string Description
+        {
+            get;
+            set;
+        }
+
+        public string LockedBy
+        {
+            get;
+            set;
+        }
+
+        public override bool Equals(object obj)
+        {
+            Product other = obj as Product;
+            
+            if (other == null)
+                return false;
+
+            if (other.Id == Guid.Empty || this.Id == Guid.Empty)
+                return false;
+
+            return other.Id == this.Id;
+        }
+
+
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+    }
+}
---
+++ ColabConcept.Web/Startup.cs
@@ -0,0 +1,23 @@
+﻿using ColabConcept.Web.Infrastructure;
+using Microsoft.AspNet.SignalR;
+using Newtonsoft.Json;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ColabConcept.Web
+{
+    public class Startup
+    {
+
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+            app.UseNancy();
+
+
+        }
+    }
+}
